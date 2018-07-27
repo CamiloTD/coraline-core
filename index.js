2ebfbd56b2735eed1328ff4a7d09d4c7710cbf7d
CamiloTD <camilotd1999@gmail.com>
2018-07-27 02:09:51 -0500

    Documentation created

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+/** @module coraline-core */
+/** 
+* Exposes ./lib/server.js
+* @param {SocketIOServer} io - The Socket.io server to use
+* @param {Object} config - Configuration object
+*/
 exports.createServer = require('./lib/server');
